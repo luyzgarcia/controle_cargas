6fe67a6d3af0ab7033d3a6d789c40cbf8cb46194
Luiz Garcia <luyzgarcia@gmail.com>
2014-11-21 14:29:10 -0200

    corrigida campos no banco, e arrumado formulario de redespachos e listagem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118153100) do
+ActiveRecord::Schema.define(version: 20141121162000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,21 +20,22 @@ ActiveRecord::Schema.define(version: 20141118153100) do
     t.string   "remetente"
     t.string   "destinatario"
     t.string   "destinatario_cidade"
-    t.decimal  "valor_redespacho",           precision: 5, scale: 2
-    t.decimal  "valor_comissao",             precision: 5, scale: 2
-    t.decimal  "valor_liquido",              precision: 5, scale: 2
-    t.decimal  "valor_frete",                precision: 5, scale: 2
-    t.decimal  "volume",                     precision: 5, scale: 2
-    t.decimal  "peso",                       precision: 5, scale: 2
-    t.decimal  "nf_valor",                   precision: 5, scale: 2
+    t.decimal  "valor_redespacho",           precision: 5, scale: 2, default: 0.0
+    t.decimal  "valor_comissao",             precision: 5, scale: 2, default: 0.0
+    t.decimal  "valor_liquido",              precision: 5, scale: 2, default: 0.0
+    t.decimal  "valor_frete",                precision: 5, scale: 2, default: 0.0
+    t.integer  "volume"
+    t.decimal  "peso",                       precision: 5, scale: 2, default: 0.0
+    t.decimal  "nf_valor",                   precision: 5, scale: 2, default: 0.0
     t.integer  "nf_numero"
     t.datetime "data_envio"
     t.datetime "data_entrega"
     t.string   "recebido_por"
     t.string   "jadlog_lista_numero"
     t.string   "jadlog_conhecimento_numero"
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
+    t.datetime "created_at",                                                       null: false
+    t.datetime "updated_at",                                                       null: false
+    t.string   "remetente_cidade"
   end
 
   create_table "users", force: true do |t|
