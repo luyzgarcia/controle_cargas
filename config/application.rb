a8377f3bb1d869e66455458eb4bd54c39606c139
Luiz Garcia <luyzgarcia@gmail.com>
2014-12-03 14:34:47 -0200

    Adicionado icones nos botoes

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,11 @@ module ControleCargas
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    
+    #config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
+    
     config.active_record.raise_in_transactional_callbacks = true
     config.to_prepare do
       Devise::SessionsController.layout "log_in"
