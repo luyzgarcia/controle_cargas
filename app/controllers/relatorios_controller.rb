e3fe1217d94306ec6483177db7d605c468267054
Luiz Garcia <luyzgarcia@gmail.com>
2014-12-12 18:33:08 -0200

    Corrigido relatórios
    Corrigido relatórios quando não filtrava ao marcar mais de uma empresa
    ou estatua.

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -32,81 +32,85 @@ class RelatoriosController < ApplicationController
   
   def filtrar
     filter = [ ]
-    parametros = params[:consulta][:registros][:f]
-    
-    if !parametros.nil? && !parametros.empty?
-            
-      if !parametros[:empresa_id].blank?
-        empresa_id = 0
-        parametros[:empresa_id].each do |emp|
-          if !emp == parametros[:empresa_id].last
-            empresa_id = ' or ' + emp  
-          end 
-          empresa_id = emp
-        end
-        filter << ["empresa_id = #{empresa_id}"]
-      end
+    if params.has_key?('consulta')
       
-      if !parametros[:status].blank?
-        status = ''
-        parametros[:status].each do |sta|
-          if !sta == parametros[:status].last
-            status = ' or ' + sta  
-          end 
-          status = sta
-        end
-        filter << ["status like '%#{status}%'"]
-      end
-
-      if !parametros[:remetente].blank?
-        remetente = parametros[:remetente]
-        filter << ["LOWER(remetente) like LOWER('%#{remetente}%')"]
-      end
+      parametros = params[:consulta][:registros][:f] 
       
-      if !parametros[:remetente_cidade].blank?
-        valor = parametros[:remetente_cidade]
-        filter << ["LOWER(remetente_cidade) like LOWER('%#{valor}%')"]
-      end
-      
-      if !parametros[:destinatario].blank?
-        valor = parametros[:destinatario]
-        filter << ["LOWER(destinatario) like LOWER('%#{valor}%')"]
-      end
-      
-      if !parametros[:destinatario_cidade].blank?
-        valor = parametros[:destinatario_cidade]
-        filter << ["LOWER(destinatario_cidade) like LOWER('%#{valor}%')"]
-      end
-      
-      if !parametros[:data_envio].blank?
-        from = Time.new
-        
-        if !parametros[:data_envio][:fr].blank?
-          from = parametros[:data_envio][:fr]
-          filter << ["data_envio >= '#{from}'"]
-        end
-        to = Time.new
-        if !parametros[:data_envio][:to].blank?
-          to = parametros[:data_envio][:to]
-          filter << ["data_envio <= '#{to}'"]
+      if !parametros.nil? && !parametros.empty?
+              
+        if !parametros[:empresa_id].blank?
+          empresa_id = '('
+          parametros[:empresa_id].each do |emp|
+            if emp != parametros[:empresa_id].last
+              empresa_id += emp + ','
+            else
+              empresa_id += emp + ')'  
+            end 
+          end
+          filter << ["empresa_id in #{empresa_id}"]
         end
         
-      end
-      
-      if !parametros[:data_entrega].blank?
-        from = Time.new
-        if !parametros[:data_entrega][:fr].blank?
-          from = parametros[:data_entrega][:fr]
-          filter << ["data_entrega >= '#{from}'"]
+        if !parametros[:status].blank?
+          status = '(|'
+          parametros[:status].each do |sta|
+            status += sta + '|'
+            if sta == parametros[:status].last
+              status += ')' 
+            end
+          end
+          filter << ["status similar to '%#{status}%'"]
         end
-        to = Time.new
-        if !parametros[:data_entrega][:to].blank?
-          to = parametros[:data_entregao][:to]
-          filter << ["data_entrega <= '#{to}'"]
+  
+        if !parametros[:remetente].blank?
+          remetente = parametros[:remetente]
+          filter << ["LOWER(remetente) like LOWER('%#{remetente}%')"]
+        end
+        
+        if !parametros[:remetente_cidade].blank?
+          valor = parametros[:remetente_cidade]
+          filter << ["LOWER(remetente_cidade) like LOWER('%#{valor}%')"]
+        end
+        
+        if !parametros[:destinatario].blank?
+          valor = parametros[:destinatario]
+          filter << ["LOWER(destinatario) like LOWER('%#{valor}%')"]
+        end
+        
+        if !parametros[:destinatario_cidade].blank?
+          valor = parametros[:destinatario_cidade]
+          filter << ["LOWER(destinatario_cidade) like LOWER('%#{valor}%')"]
+        end
+        
+        if !parametros[:data_envio].blank?
+          from = Time.new
+          
+          if !parametros[:data_envio][:fr].blank?
+            from = parametros[:data_envio][:fr]
+            filter << ["data_envio >= '#{from}'"]
+          end
+          to = Time.new
+          if !parametros[:data_envio][:to].blank?
+            to = parametros[:data_envio][:to]
+            filter << ["data_envio <= '#{to}'"]
+          end
+          
+        end
+        
+        if !parametros[:data_entrega].blank?
+          from = Time.new
+          if !parametros[:data_entrega][:fr].blank?
+            from = parametros[:data_entrega][:fr]
+            filter << ["data_entrega >= '#{from}'"]
+          end
+          to = Time.new
+          if !parametros[:data_entrega][:to].blank?
+            to = parametros[:data_entregao][:to]
+            filter << ["data_entrega <= '#{to}'"]
+          end
+          
         end
         
       end
-      
     end
     
     @registros = Redespacho.where(filter.join(" AND ")).order('created_at DESC')
