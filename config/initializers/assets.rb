93b81daf37562aeb266e9c78d3f94a5ccccb55e9
Luiz Garcia <luyzgarcia@gmail.com>
2015-02-10 14:58:07 -0200

    Alterações na tela de cadastro de empresa
    Alterações na tela de cadastro de empresa

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 #Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.precompile += %w( empresas.js )
+Rails.application.config.assets.precompile += %w( *.png )
