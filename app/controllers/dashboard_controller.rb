c2de9766259671cbe94a520a6c894a97888e1170
Luiz Garcia <luyzgarcia@gmail.com>
2015-02-19 17:47:01 -0200

    correcao no dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @registros = Redespacho.all
-    
-    respond_to do |format|
-      format.xml {render xml: @registros}
-      format.json {render json: @registros}
-      format.html
-    end
     
   end
 end
